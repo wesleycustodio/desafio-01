7cad52e92b2bf05d3f45f8e0fe2c00e69ebc753a
wesley <wesley.sergio.custodio@gmail.com>
2023-11-26 13:23:17 -0300

    desafio01

---
+++ app.js
@@ -0,0 +1,11 @@
+let resposta = prompt('Qual dia da semana? ');
+let semana = "Sabado";
+let semana1 = "Domingo"
+
+if (resposta == semana) {
+    alert('Bom final de semana! ')
+}else if (resposta == semana1 ) {
+    alert('Bom final de semana! ')
+}else{
+    alert('Boa Semana! ')
+}
